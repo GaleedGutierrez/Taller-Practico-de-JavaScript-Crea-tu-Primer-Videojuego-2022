fbf2da8b59c15f58f24a99cf6e0993d5a4ae90e0
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-10-25 22:56:21 -0300

    :white_check_mark: Clase N°1: Programemos un juego con JavaScript.
    Cree mis propios estilos para el proyecto.

---
+++ dist/maps.js
@@ -0,0 +1,65 @@
+"use strict";
+/*
+ * Reglas:
+ * El final de cada nivel debe ser el inicio del siguiente
+*/
+const EMOJIS = {
+    '-': ' ',
+    'O': '🚪',
+    'X': '💣',
+    'I': '🎁',
+    'PLAYER': '💀',
+    'BOMB_COLLISION': '🔥',
+    'GAME_OVER': '👎',
+    'WIN': '🏆',
+    'LIFE': '❤️'
+};
+const thoEMOJIS = {
+    '-': ' ',
+    'O': '💻',
+    'X': '👾',
+    'I': '✅',
+    'PLAYER': '👨‍💻',
+    'BOMB_COLLISION': '💥',
+    'GAME_OVER': '👎',
+    'WIN': '🏆',
+    'LIFE': '❤️'
+};
+const MAPS = [];
+MAPS.push(`
+    IXXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    OXXXXXXXXX
+  `);
+MAPS.push(`
+    O--XXXXXXX
+    X--XXXXXXX
+    XX----XXXX
+    X--XX-XXXX
+    X-XXX--XXX
+    X-XXXX-XXX
+    XX--XX--XX
+    XX--XXX-XX
+    XXXX---IXX
+    XXXXXXXXXX
+    `);
+MAPS.push(`
+    I-----XXXX
+    XXXXX-XXXX
+    XX----XXXX
+    XX-XXXXXXX
+    XX-----XXX
+    XXXXXX-XXX
+    XX-----XXX
+    XX-XXXXXXX
+    XX-----OXX
+    XXXXXXXXXX
+  `);
+//# sourceMappingURL=maps.js.map
